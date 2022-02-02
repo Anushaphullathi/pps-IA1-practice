c28e51f48f5ed8f2eb5a6e30b95a75e1a5f2883e
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-02-02 04:39:20 +0000

    p1

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
-
+#include<stdio.h>
+void input(int *a,int *b)
+{
+  printf("enter the value of two numbers\n");
+  scanf("%d %d",a,b);
+}
+void add(int a,int b,int *sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("%d+%d is %d\n",a,b,sum);
+}
+int main()
+{
+  int p,q,sum;
+  input(&p,&q);
+  add(p,q,&sum);
+  output(p,q,sum);
+  return 0;
 }
